808da49792cccd9a81aef0c5edcdcde340005b88
Suncheol Kim <tinymin@yahoo.co.kr>
2020-08-24 22:25:14 +0900

    First commit

---
+++ src/main/java/net/tinymin/atm/AutomalltaApplication.java
@@ -0,0 +1,14 @@
+package net.tinymin.atm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomalltaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomalltaApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/net/tinymin/atm/AutomalltaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.tinymin.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomalltaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
